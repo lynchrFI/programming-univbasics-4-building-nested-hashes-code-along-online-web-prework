a9c727d8dce3a6646439fc1d3a80012343437feb
Ryan Lynch <lynchrbusiness@gmail.com>
2019-09-29 20:47:47 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -20,7 +20,7 @@ def third_challenge
         "Lady Capulet", age: "51"
         },
       :heroine => {
-        
+        "Juliet", age: "15", status: "alive"
         },
       :heroine_friends => []
    }
