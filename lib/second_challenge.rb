fc6126dd0ec666f732b438e0277a53f53f7ab2e7
Ryan Lynch <lynchrbusiness@gmail.com>
2019-09-29 20:43:58 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -26,7 +26,30 @@ def second_challenge
         :name => "Lady Capulet",
         :age => "50"
       
-    
+      },
+    :matriarch => {
+      :name => "Lady Capulet",
+      :age => "51"
+    },
+    :heroine => {
+      :name => "Juliet",
+      :age => "15",
+      :status => "alive"
+    },
+    :heroine_friends=> [
+      {
+        :name => "Steven",
+        :age => "30",
+        :attitude => "confused"
+      },
+      {
+        :name => "Nurse",
+        :age => "44",
+        :attitude => "worried"
+      }
+    ]
+  }
+}
 
   
 end
